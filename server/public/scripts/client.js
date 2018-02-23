b65f578819a114d18d03f5e9808e967716089c80
Sam Standing <samzstanding@gmail.com>
2018-02-23 15:33:32 -0600

    started server and crnd created files

---
+++ server/public/scripts/client.js
@@ -0,0 +1,14 @@
+const app = angular.module('myApp', []);
+
+const appController = app.controller('AppController', ['$http', function ($http) {
+let self = this;
+console.log('what up!');
+
+
+
+
+
+
+
+
+}]);//end app Controller
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('server/public'));
+
+let port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+    console.log(`listening on port ${port}`);
+    
+})
