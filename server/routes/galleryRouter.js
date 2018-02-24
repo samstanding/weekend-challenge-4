e9dd4baafd28c259b9310374a891acdc6c6e5f84
Sam Standing <samzstanding@gmail.com>
2018-02-24 12:49:09 -0600

    created router, pool and table files

---
+++ server/routes/galleryRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../modules/pool');
+const bodyParser = require('body-parser');
+
+
+module.exports = router;
