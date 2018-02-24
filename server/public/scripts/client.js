ff55a7917982224e915e3aa98d9f416a36836d27
Sam Standing <samzstanding@gmail.com>
2018-02-24 13:27:16 -0600

    sending all photo info from db now

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,26 @@ const appController = app.controller('AppController', ['$http', function ($http)
 let self = this;
 console.log('what up!');
 
+self.photoArray = [];
+
+
+
+self.getPhotos = function () {
+    $http ({
+        method:'GET',
+        url: '/gallery'
+    }).then (function (response) {
+        self.photoArray= response.data.rows;
+        console.log(self.photoArray);
+    }).catch(function (error) {
+        console.log(error, 'on get');
+    })
+}
+
+self.getPhotos();
+
+
+
 
 
 
--- server/routes/galleryRouter.js
+++ server/routes/galleryRouter.js
@@ -4,5 +4,15 @@ const router = express.Router();
 const pool = require('../modules/pool');
 const bodyParser = require('body-parser');
 
+router.get('/', function (req, res) {
+    const sqlText = `SELECT * FROM photos`;
+    pool.query(sqlText) 
+    .then(function (result) {
+        res.send(result);
+    }).catch(function (error) {
+        console.log('on get: ',error);
+    })
+});
+
 
 module.exports = router;
