f04f224041aeba4fd823bdbb945044162b8ae626
Sam Standing <samzstanding@gmail.com>
2018-02-25 22:09:51 -0600

    added .sql file

---
+++ gallery.sql
@@ -0,0 +1,16 @@
+CREATE TABLE photos ( 
+	id serial primary key,
+	img VARCHAR (1000),
+    like_count integer DEFAULT 0,
+	view_count integer DEFAULT 0,
+    description VARCHAR (600),
+    title VARCHAR (80)
+);
+
+
+
+CREATE TABLE comments (
+	comments VARCHAR (500),
+	photo_id integer references photos,
+	author VARCHAR (80)
+	);
